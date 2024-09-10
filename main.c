9cf3cb7bfb2febbab67e615ede1a96ddfeee5342
Gagana-shre <gaganajgagana@gmail.com>
2024-09-10 15:58:41 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+  printf("Enter a number\n");
+  scanf("%d",&n);
+  if(n>=0)
+    printf("positive\n");
+  else
+    printf("Negative\n");
+  return 0;
+}
